6a2475b8d7f5867119c432a535d610e22f9b0e85
Andrei Popa <35630083+andreipopa90@users.noreply.github.com>
2021-12-12 12:32:49 +0100

    Updated Day 12 solution

--- src/Day12.cpp
+++ src/Day12.cpp
@@ -11,57 +11,39 @@ using namespace std;
 
 struct Cave {
     string name;
-    bool visited;
     bool small;
     vector<Cave> neighbours;
 };
 
 vector<Cave> path;
-vector<string> cave_names;
-vector<Cave> cave_system;
+map<string, Cave> caves;
 map<string, int> visited;
 bool twice = false;
 int nb_of_paths = 0;
 
-void print_path() {
-    nb_of_paths ++;
-}
+void walk(const Cave &cave) {
 
-void walk(Cave &cave) {
-    int index;
+    if (cave.name == "end") {
+        nb_of_paths ++;
+    } else if (((!twice) || (twice && visited[cave.name] == 0))) {
 
-    if (!cave.visited) {
         if (cave.small) {
             visited[cave.name] += 1;
-            if (twice) {
-                cave.visited = true;
-            }
             if (visited[cave.name] == 2) {
-                cave.visited = true;
                 twice = true;
-                for (auto &c: visited) {
-                    index = distance(cave_names.begin(), find(cave_names.begin(), cave_names.end(), c.first));
-                    cave_system[index].visited = c.second > 0;
-                }
             }
         }
         path.push_back(cave);
 
-        if (cave.name == "end") {
-            print_path();
+        for (auto &n: cave.neighbours) {
+            walk(caves[n.name]);
         }
 
-        for (auto &n: cave.neighbours) {
-            index = distance(cave_names.begin(), find(cave_names.begin(), cave_names.end(), n.name));
-            walk(cave_system[index]);
-        }
         if (cave.small) {
             visited[cave.name] -= 1;
             if (visited[cave.name] == 1) {
                 twice = false;
-                for (auto &c: cave_system) c.visited = false;
             }
-            cave.visited = false;
         }
         path.pop_back();
     }
@@ -77,44 +59,41 @@ int main() {
 
     while (getline(fin, input)) {
         link1.name = input.substr(0, input.find(delim));
-        link1.visited = false;
         link1.small = 'a' <= link1.name[0] && link1.name[0] <= 'z';
+
         link2.name = input.substr(input.find(delim) + 1, input.size());
-        link2.visited = false;
         link2.small = 'a' <= link2.name[0] && link2.name[0] <= 'z';
 
         links.push_back(make_pair(link1.name, link2.name));
 
-        if (find(cave_names.begin(), cave_names.end(), link1.name) == cave_names.end()) {
-            cave_names.push_back(link1.name);
-            cave_system.push_back(link1);
+        if (caves.find(link1.name) == caves.end()) {
+            caves[link1.name] = link1;
         }
 
-        if (find(cave_names.begin(), cave_names.end(), link2.name) == cave_names.end()) {
-            cave_names.push_back(link2.name);
-            cave_system.push_back(link2);
+        if (caves.find(link2.name) == caves.end()) {
+            caves[link2.name] = link2;
         }
     }
+
+    fin.close();
 
     for (auto pair: links) {
-        int index1 = distance(cave_names.begin(), find(cave_names.begin(), cave_names.end(), pair.first));
-        int index2 = distance(cave_names.begin(), find(cave_names.begin(), cave_names.end(), pair.second));
 
-        if (cave_names[index1] != "end" && cave_names[index2] != "start") {
-            cave_system[index1].neighbours.push_back(cave_system[index2]);
+        if (pair.first != "end" && pair.second != "start") {
+            caves[pair.first].neighbours.push_back(caves[pair.second]);
         }
-        if (cave_names[index2] != "end" && cave_names[index1] != "start") {
-            cave_system[index2].neighbours.push_back(cave_system[index1]);
+        if (pair.second != "end" && pair.first != "start") {
+            caves[pair.second].neighbours.push_back(caves[pair.first]);
         }
     }
 
-    for (auto c: cave_names) {
-        if (c != "start" and c != "end") {
-            visited[c] = 0;
+    for (auto c: caves) {
+        if (c.first != "start" and c.first != "end") {
+            visited[c.first] = 0;
         }
     }
 
-    walk(cave_system[distance(cave_names.begin(), find(cave_names.begin(), cave_names.end(), "start"))]);
+    walk(caves["start"]);
     cout << nb_of_paths << endl;
     return 0;
 }
